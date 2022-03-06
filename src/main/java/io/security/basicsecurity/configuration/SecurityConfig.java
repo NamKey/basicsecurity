eba3bbaa8e0c568a4e8e4657b4f7b14b83723c35
KeyNam <rlfbd5142@gmail.com>
2022-03-06 16:17:20 +0900

    feature : CustomSecurity 설정하기
    - CustomSecurity 설정

---
+++ src/main/java/io/security/basicsecurity/configuration/SecurityConfig.java
@@ -0,0 +1,44 @@
+package io.security.basicsecurity.configuration;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated(); // 인가정책
+
+        // loginPage도 인증을 받아야 들어갈수 있음
+        http.formLogin()
+//                .loginPage("/loginPage")
+                .defaultSuccessUrl("/")
+                .failureUrl("/login")
+                .usernameParameter("userId")
+                .passwordParameter("passwd")
+                .loginProcessingUrl("/login_proc")
+                .successHandler((request, response, authentication) -> {
+                    System.out.println("authentication = " + authentication.getName());
+                    response.sendRedirect("/");
+                })
+                .failureHandler((request, response, exception) -> {
+                    System.out.println("exception = " + exception.getMessage());
+                    response.sendRedirect("/login");
+                })
+                .permitAll();
+    }
+}
